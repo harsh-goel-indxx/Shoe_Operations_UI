54387de5cf43dd47712d33d735f65620300cdb2d
Harsh Goel <hgoel@gmail.com>
2026-06-06 10:46:09 +0530

    Fix:Change base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "http://3.110.108.83:8000/api/",
   headers: { "Content-Type": "application/json" },
 });
 
